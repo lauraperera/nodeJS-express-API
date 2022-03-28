de33217eeabfb27860e74b7df4f3a9773dce7d95
Laura Perera <lauraperera.pss@gmail.com>
2022-03-28 11:40:25 -0300

    Update get client by id

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const data = require("./data.json"); 
+//arquivo clientsData.json simula um database
+const clientsData = require("./clientsData.json"); 
 
 app.use(express.json());
 
 app.get('/clients', (req, res)=>{
-    res.json(data);
+    res.json(clientsData);
 });
+
 //recebe 1 só client pelo id
 app.get('/clients/:id', (req, res)=>{
-    const {id} = req.params
-    res.json(data);
+    const {id} = req.params;
+    const client = clientsData.find(client => client.id == id);
+    res.json(client);
 }); 
-app.post('/clients', (req, res)=>{
 
+app.post('/clients', (req, res)=>{
+    res.json(clientsData);
 });
+
 //só pode atualizar 1 client
 app.put('/clients/:id', (req, res)=>{
-
+    
 }); 
 //só pode deletar 1 client
 app.delete('/clients/:id', (req, res)=>{
